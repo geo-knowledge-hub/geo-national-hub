9b0177a846141f0c080d471cabb2ea113d144994
Felipe Carlos <efelipecarlos@gmail.com>
2025-05-05 00:06:11 +0200

    global: update path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/national',
+  assetPrefix: '/national',
   output: 'standalone',
   images: {
     domains: ['earthobservations.org'],
