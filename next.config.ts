563e8c5e0a2c80a207117913f51e90b1315b7a8c
Felipe Carlos <efelipecarlos@gmail.com>
2026-01-29 05:46:24 -0300

    global: update next config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,20 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['earthobservations.org'],
   },
+  env: {
+    TYPESENSE_HOST: process.env.TYPESENSE_HOST,
+    TYPESENSE_PORT: process.env.TYPESENSE_PORT,
+    TYPESENSE_PROTOCOL: process.env.TYPESENSE_PROTOCOL,
+    TYPESENSE_API_KEY: process.env.TYPESENSE_API_KEY,
+  },
 };
 
+/**
+ * Force dynamic rendering
+ */
+export const dynamic = 'force-dynamic';
+
+/**
+ * Export the next config
+ */
 export default nextConfig;
